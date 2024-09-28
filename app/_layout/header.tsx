8db4d062a9f44fc9cb323747f2b9195898b7fb39
jjalseu <cjfwns966@gmail.com>
2024-09-28 13:14:52 +0900

    - 레이아웃 > 헤더 - tailwind.config.ts > color,fontsize setting - 버튼 컴포넌트

--- app/_layout/header.tsx
+++ app/_layout/header.tsx
@@ -1,12 +1,67 @@
+"use client"
+
 import React from 'react';
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import PrimaryButton from "@/app/_components/button/primaryButton";
 
 /**
  * 헤더
  */
 const Header = () => {
+    const pathname = usePathname();
+
+
+    // 네비게이션 메뉴
+    const NAVMENU = [
+        {
+            title: "퀴즈",
+            link: "quiz",
+        },
+        {
+            title: "코드 템플릿",
+            link: "codeTemplate",
+        },
+        {
+            title: "스터디",
+            link: "study",
+        },
+    ]
+
+
+    // 현재 페이지에 맞는 클래스명
+    function getActiveClass(link:string){
+        if(pathname===`/${link}`){
+            return "bg-primary-normal text-white hover:bg-primary-dark "
+        }
+    }
     return (
-        <header>
-            <h1>Dev Lounge</h1>
+        <header className={"w-full h-[96px] bg-background flex justify-between items-center px-[32px]"}>
+            {/*로고,메뉴*/}
+            <div className={"flex gap-28 items-center"}>
+                <Link href={"/"}>
+                <h1 className={"text-primary-normal text-title1"}>Dev Lounge</h1>
+                </Link>
+                <nav>
+                    <ul className={"flex gap-3 text-title3Normal text-primary-normal"}>
+                        {NAVMENU.map((item, index) => (
+                            <li key={index} className={`cursor-pointer px-[12px] flex justify-center items-center w-[100px]  h-[32px] rounded-[8px] hover:bg-primary-dark hover:text-white ${getActiveClass(item.link)}`}>
+                                <Link href={`/${item.link}`}>{item.title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+            {/* 로그인,회원가입 & 프로필 */}
+            <div className={"flex gap-3 items-center"}>
+                <Link href={"/login"}>
+                    <PrimaryButton text={"로그인"} color={"primary"} />
+                </Link>
+                <Link href={"/signup"}>
+                    <PrimaryButton text={"회원가입"} color={"primarySecondary"} />
+                </Link>
+
+            </div>
         </header>
     );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,98 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
+        background: "#333333",
         foreground: "var(--foreground)",
+        primary:{
+          // PRIMARY
+          normal:"#FF6F00",
+          light:"#FF8C33",
+          dark:"#E65A00",
+          // SECONDARY
+          secondary:{
+            normal:"#8A75FF",
+            light:"#B7A3FF",
+            dark:"#5F4DFF",
+          }
+        },
+        white:"#FFFFFF",
       },
+      fontSize: {
+        headline1:[
+          "72px",
+          {
+            fontWeight:"600"
+          },
+        ],
+        headline2:[
+          "56px",
+          {
+            fontWeight:"600"
+          },
+        ],
+
+        headline3:[
+          "32px",
+          {
+            fontWeight:"600"
+          },
+        ],
+        title1:[
+          "24px",
+          {
+            fontWeight:"600"
+          },
+        ],
+        title2Bold:[
+          "18px",
+          {
+            fontWeight:"600"
+          },
+        ],
+        title2Normal:[
+          "18px",
+          {
+            fontWeight:"500"
+          },
+        ],
+        title3Bold:[
+          "16px",
+          {
+            fontWeight:"600"
+          },
+        ],
+        title3Normal:[
+          "16px",
+          {
+            fontWeight:"400"
+          },
+        ],
+        menu:[
+          "15px",
+          {
+            fontWeight:"500"
+          },
+        ],
+        label1:[
+          "13px",
+          {
+            fontWeight:"500"
+          },
+        ],
+        label2:[
+          "12px",
+          {
+            fontWeight:"400"
+          },
+        ],
+        body:[
+          "14px",
+          {
+            fontWeight:"400"
+          },
+        ]
+      },
+
     },
   },
   plugins: [],
