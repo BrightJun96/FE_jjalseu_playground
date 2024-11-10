98293599eee9599dddd0018b84429c3ce4346653
jjalseu <cjfwns966@gmail.com>
2024-11-10 16:15:20 +0900

    - 퀴즈 상세 페이지 > 정적 렌더링 방식 요구사항 설정

--- app/(page)/quiz/details/page.tsx
+++ app/(page)/quiz/details/page.tsx
@@ -7,6 +7,10 @@ import QuizDetails from "@/app/(page)/quiz/details/components/client/quizDetails
 
 /**
  * 퀴즈 문제 페이지
+ * 정적 렌더링 방식
+ * @todo SSG 방식으로 변경 필요
+ * @todo API로부터 퀴즈 시작하기 옵션에 따른 PK 목록들을 받아와 해당 PK들에 대한 페이지를 SSG로 생성하여 보여준다.
+ * @todo SEO를 위해 title,description 등 값 설정 필요
  */
 const Page = async ({searchParams}:{
     searchParams:Promise<{ [key: string]: string | string[] | undefined }>
