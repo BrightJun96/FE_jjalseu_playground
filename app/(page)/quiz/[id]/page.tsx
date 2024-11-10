b94a114caa430e00259a3f1bdce4141aa3a7bcf3
jjalseu <cjfwns966@gmail.com>
2024-11-10 19:46:17 +0900

    - quizDetails.tsx 퀴즈 상세 페이지 > 메타 데이터 설정 셋팅 >generateMetadata에서 상세 API 호출

--- app/(page)/quiz/[id]/page.tsx
+++ app/(page)/quiz/[id]/page.tsx
@@ -1,3 +1,4 @@
+import {Metadata} from "next";
 import React from 'react';
 import {fetchQuizDetail, fetchQuizDetailPk, fetchQuizList, fetchQuizPkList} from "@/app/services/quiz/api.instance";
 import {cookies} from "next/headers";
@@ -8,16 +9,35 @@ import QuizDetails from "@/app/(page)/quiz/[id]/components/client/quizDetails";
 /**
  * 퀴즈 문제 페이지
  * 정적 렌더링 방식
- * @todo SSG 방식으로 변경 필요
- * @todo API로부터 퀴즈 시작하기 옵션에 따른 PK 목록들을 받아와 해당 PK들에 대한 페이지를 SSG로 생성하여 보여준다.
  * @todo SEO를 위해 title,description 등 값 설정 필요
+ * @todo SEO 노출을 위해 어떤값들을 설정하면 되는지 알아보기
  */
 
+// SSG 실행할 페이지 ID 추출, 서버에 받아오는 PK들은 모두 SSG 방식으로 구현
 export async function generateStaticParams() {
 
     const {data} = await fetchQuizPkList()
 
     return data.map((pk) => ({id:pk.toString()}))
+}
+
+// SEO를 위해 설정
+export async function generateMetadata({
+                                           params
+                                       }:{
+    params:{
+        id:string
+    }
+}):Promise<Metadata>{
+
+    const id = (await params).id
+
+    const {data} = await fetchQuizDetailPk(Number(id))
+
+    return {
+        title:"퀴즈",
+        description:"퀴즈 설명"
+    }
 }
 
 const Page = async ({
@@ -31,7 +51,6 @@ const Page = async ({
     const { id } = await params
     const {data} = await fetchQuizDetailPk(Number(id))
 
-    console.log("data :",data)
 
     return (
         <QuizDetails
