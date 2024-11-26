bc2f10981b8d0d833ec59c480bd5c8bc2fba3c52
jjalseu <cjfwns966@gmail.com>
2024-11-26 23:30:20 +0900

    - quizOptionSettingPart.tsx > quizStart 함수 quizHelper.ts class로 분리

---
+++ app/(page)/quiz/[detailUrl]/_helper/QuizHelper.ts
@@ -0,0 +1,42 @@
+// 퀴즈 페이지에서 사용할 헬퍼 함수들
+import {ArrayUtils} from "@/app/_utils/function/ArrayUtils";
+import {StorageService} from "@/app/_utils/StorageService";
+import {clientQuizApi} from "@/app/services/quiz/client/api.instance";
+
+class QuizHelper{
+
+    // 배열에서 랜덤한 요소를 반환
+    pickRandomOne<T>(arr: T[]): T  {
+            if (arr.length === 0) {
+                throw new Error("배열이 비어있습니다. 랜덤 선택이 불가능합니다.");
+            }
+
+        return ArrayUtils.pickRandomOne(arr);
+    }
+
+    // 퀴즈 시작하기
+    async startQuiz(storage:StorageService,navigate:(path:string) => void){
+        try {
+            // 퀴즈 URL 목록 조회
+            const {data} = await clientQuizApi.fetchQuizDetailUrlList()
+
+            // 퀴즈 URL 목록을 localStorage에 저장
+            storage.save("quizUrlList", JSON.stringify(data))
+
+            // 데이터 중 랜덤으로 하나뽑기
+            const randomOne = this.pickRandomOne<string>(data)
+
+            // 랜덤으로 뽑은 퀴즈 URL로 이동
+            navigate(`/quiz/${randomOne}`)
+
+        }
+        catch (error){
+            console.error(error)
+        }
+
+    }
+}
+
+const quizHelper = new QuizHelper();
+
+export default quizHelper;
--- app/(page)/quiz/_components/quizOptionSettingPart.tsx
+++ app/(page)/quiz/_components/quizOptionSettingPart.tsx
@@ -1,14 +1,14 @@
 "use client"
 
-import quizHelper from "@/app/(page)/quiz/[detailUrl]/_helper/quizHelper";
+import quizHelper from "@/app/(page)/quiz/[detailUrl]/_helper/QuizHelper";
 import {FIELD_OPTIONS, LANGUAGE_OPTIONS} from "@/app/(page)/quiz/constant";
 import PrimaryButton from "@/app/_components/button/primaryButton";
 import Select from "@/app/_components/select/select";
-import {localStorageService, StorageService} from "@/app/_utils/StorageService";
-import {clientQuizApi} from "@/app/services/quiz/client/api.instance";
+import {localStorageService} from "@/app/_utils/StorageService";
 import {useRouter} from "next/navigation";
 import React from 'react';
 
+// 퀴즈 옵션 설정 컴포넌트
 function QuizOptionSettingPart() {
 
     const [option,setOption] = React.useState<{field:string,lang:string}>({field:FIELD_OPTIONS[0].value,lang:LANGUAGE_OPTIONS[0].value});
@@ -20,27 +20,7 @@ function QuizOptionSettingPart() {
         setOption({...option,[key]:value})
     }
 
-    async function handleStartQuiz(storage:StorageService){
 
-        try {
-            // 퀴즈 URL 목록 조회
-            const {data} = await clientQuizApi.fetchQuizDetailUrlList()
-
-            // 퀴즈 URL 목록을 localStorage에 저장
-            storage.save("quizUrlList", JSON.stringify(data))
-
-            // 데이터 중 랜덤으로 하나뽑기
-            const randomOne = quizHelper.pickRandomOne<string>(data)
-
-            // 랜덤으로 뽑은 퀴즈 URL로 이동
-            router.push(`/quiz/${randomOne}`)
-
-        }
-        catch (error){
-            console.error(error)
-        }
-
-    }
     return (
         <>
             <div className={"flex flex-col gap-10 w-full"}>
@@ -57,7 +37,7 @@ function QuizOptionSettingPart() {
                 />
             </div>
                 <PrimaryButton
-                    onClick={async() => handleStartQuiz(localStorageService)}
+                    onClick={async() => quizHelper.startQuiz(localStorageService,router.push)}
                     text={"시작하기"}
                     color={"primary"}
                     className={"!w-full !h-[48px] !mt-14"}/>
