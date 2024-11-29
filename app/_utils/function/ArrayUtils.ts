831f4689824001830ea41206558a2e5a150e1726
jjalseu <cjfwns966@gmail.com>
2024-11-29 14:39:21 +0900

    - ArrayUtils.ts 클래스

--- app/_utils/function/ArrayUtils.ts
+++ app/_utils/function/ArrayUtils.ts
@@ -6,4 +6,17 @@ export class ArrayUtils{
         const randomIndex = Math.floor(Math.random() * arr.length);
         return arr[randomIndex];
     }
+
+    // 두 배열을 비교하여,A배열 기준으로 B배열 요소를 제외한 배열을 반환
+    static getDifference<T>(arrA: T[], arrB: T[]): T[] {
+        return arrA.filter((a) => !arrB.includes(a));
+    }
+
+    // 배열 길이가 0일 때, 특정 함수를 실행
+    static runIfEmpty<T>(arr: T[], func: () => void): void {
+        if (arr.length === 0) {
+            func();
+        }
+    }
+
 }
