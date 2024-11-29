248f431ad0594b4f6f35173fd2a5d80d521137ac
jjalseu <cjfwns966@gmail.com>
2024-11-29 14:54:24 +0900

    - 퀴즈 완성 페이지

---
+++ app/(page)/quiz/completed/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// 퀴즈를 다 풀었을 때, 페이지
+function Page() {
+    return (
+        <div>
+            <h1>퀴즈 완료</h1>
+            <p>축하드립니다. 모든 퀴즈를 다 푸셨습니다.</p>
+        </div>
+    );
+}
+
+export default Page;
