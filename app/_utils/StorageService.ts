e8c94e2845fb2ae0073e6d7fa1fa15f93da6ab18
jjalseu <cjfwns966@gmail.com>
2024-11-29 13:27:15 +0900

    - StorageService.ts > getParsed 메서드 추가

--- app/_utils/StorageService.ts
+++ app/_utils/StorageService.ts
@@ -5,23 +5,43 @@
 export interface StorageService {
     save(key: string, value: string): void; // 저장
     remove(key:string):void // 제거
+    get(key: string): string | null; // 가져오기
+    getParsed<T>(key: string,valueInNullCase:unknown): T | unknown; // 파싱하여 가져오기
 }
 // 스토리지 서비스 구현 클래스
  export class StorageAdapter implements StorageService {
     private storage: Storage;
+
     constructor( storage: Storage) {
         this.storage = storage;
     }
 
+    // 저장
     save(key: string, value: string) {
         this.storage.setItem(key, value);
     }
 
+    // 제거
     remove(key: string) {
         this.storage.removeItem(key);
     }
 
+    // 가져오기
+    get(key: string): string | null {
+        return this.storage.getItem(key);
+    }
 
+    // 파싱하여 가져오기
+     getParsed<T>(key: string,valueInNullCase:unknown): T | unknown { {
+        const value = this.get(key);
+        if (value) {
+            return JSON.parse(value);
+        }
+        return valueInNullCase;
+
+     }
+
+    }
 }
 
 
