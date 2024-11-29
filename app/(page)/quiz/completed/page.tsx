250f36527b932530b47feca29ee32c702fe89e2f
jjalseu <cjfwns966@gmail.com>
2024-11-29 14:58:26 +0900

    퀴즈 완료 페이지 > 퀴즈 풀러가기 버튼 추가

--- app/(page)/quiz/completed/page.tsx
+++ app/(page)/quiz/completed/page.tsx
@@ -1,3 +1,5 @@
+import PrimaryButton from "@/app/_components/button/primaryButton";
+import Link from "next/link";
 import React from 'react';
 
 // 퀴즈를 다 풀었을 때, 페이지
@@ -6,6 +8,9 @@ function Page() {
         <div>
             <h1>퀴즈 완료</h1>
             <p>축하드립니다. 모든 퀴즈를 다 푸셨습니다.</p>
+            <Link href={"/quiz"}>
+                <PrimaryButton text={"다른 퀴즈 풀러가기"} color={"primary"}/>
+            </Link>
         </div>
     );
 }
