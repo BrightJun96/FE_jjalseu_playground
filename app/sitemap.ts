8afc9bda8009c748675f1d0daef39c2cb659da1a
jjalseu <cjfwns966@gmail.com>
2024-11-30 17:11:24 +0900

    - sitemap todo 주석

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,12 +6,14 @@ export default async function sitemap():Promise<MetadataRoute.Sitemap>{
     return [
         {
             url: BASE_URL,
+            // @todo 마지막 수정날짜로 변경
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.5,
         },
         {
             url: `${BASE_URL}/quiz`,
+            // @todo 마지막 수정날짜로 변경
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
