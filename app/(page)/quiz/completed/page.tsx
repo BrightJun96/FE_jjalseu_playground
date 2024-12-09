f6eb28261b5f0fadd6ec67a1d41a1f940aed4ee4
jjalseu <cjfwns966@gmail.com>
2024-12-09 19:38:03 +0900

    - 퀴즈 완료 페이지 > 정적 페이지 명시

--- app/(page)/quiz/completed/page.tsx
+++ app/(page)/quiz/completed/page.tsx
@@ -5,6 +5,7 @@ import useQuizHelperContext from "@/app/_context/useQuizContext";
 import Link from "next/link";
 import React from 'react';
 
+export const dynamic = 'force-static'
 
 // @question 페이지 자체를 클라이언트 컴포넌트로 만드는 거랑 서버컴포넌트로 만드는 것이랑 차이?
 // 퀴즈를 다 풀었을 때, 페이지
@@ -12,7 +13,7 @@ function Page() {
 
     const quizHelper = useQuizHelperContext()
     return (
-        <div>
+        <>
             <h1>퀴즈 완료</h1>
             <p>축하드립니다. 모든 퀴즈를 다 푸셨습니다.</p>
             <Link href={"/quiz"}>
@@ -22,7 +23,7 @@ function Page() {
                     onClick={() => quizHelper?.clearQuizStorage()  }
                 />
             </Link>
-        </div>
+        </>
     );
 }
 
