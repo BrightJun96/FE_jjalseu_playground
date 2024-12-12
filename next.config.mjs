848601d0fc21df8be5ef91534fc812ce275dca9d
jjalseu <cjfwns966@gmail.com>
2024-12-12 19:35:28 +0900

    - 번들 분석 도구 패키지 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
-const nextConfig = {
-    // logging:{
-    //     fetches:{
-    //         fullUrl: true,
-    //     }
-    // }
-};
+/** @type {import('next').NextConfig} */
+import withBundleAnalyzer from '@next/bundle-analyzer';
 
-// export default withPWA({...nextConfig,dest: 'build'});
-export default  nextConfig
+const nextConfig = {};
+
+const bundleAnalyzer = withBundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
+});
+
+export default bundleAnalyzer(nextConfig);
