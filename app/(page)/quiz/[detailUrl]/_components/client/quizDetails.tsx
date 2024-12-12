349734bc6c3fb4c9857288f689021ec62a03ad85
jjalseu <cjfwns966@gmail.com>
2024-12-12 22:37:37 +0900

    - 퀴즈 상세 페이지 -> 클라이언트 컴포넌트,서버 컴포넌트 분리

--- app/(page)/quiz/[detailUrl]/_components/client/quizDetails.tsx
+++ app/(page)/quiz/[detailUrl]/_components/client/quizDetails.tsx
@@ -1,40 +1,24 @@
-"use client"
+"use server"
 
 import QuizAnswerForm from "@/app/(page)/quiz/[detailUrl]/_components/client/quizAnswerForm";
 import QuizContent from "@/app/(page)/quiz/[detailUrl]/_components/client/quizContent";
+import QuizDetailsManager from "@/app/(page)/quiz/[detailUrl]/_components/client/quizDetailsManager";
 import QuizQuestion from "@/app/(page)/quiz/[detailUrl]/_components/client/quizQuestion";
 import QuizTitle from "@/app/(page)/quiz/[detailUrl]/_components/client/quizTitle";
 
 import 'prismjs/themes/prism.css';
-import useQuizHelperContext from "@/app/_context/useQuizContext";
 import {QuizItem} from "@/app/services/quiz/types";
-import {useParams} from "next/navigation";
-import React, {useEffect} from 'react';
+import React from 'react';
 
 // 퀴즈 상세 컴포넌트
 const QuizDetails = ({
                          quizData
                      }:{quizData:QuizItem}) => {
 
-    const {detailUrl} = useParams()
-    const quizHelper = useQuizHelperContext();
-
-
-    useEffect(() => {
-
-        // 현재 경로가 solvedQuizList 스토리지에 있는 url에 있는 경우, 다른 문제로 이동
-        quizHelper?.redirectIfQuizSolved(detailUrl as string)
-
-        // 모든 퀴즈를 푼 경우, 퀴즈 완료 페이지로 이동
-        quizHelper?.redirectToCompletionPageIfAllSolved()
-
-    }, [detailUrl])
-
-
 
 
     return (
-        <>
+        <QuizDetailsManager>
             {/*퀴즈 제목*/}
             <QuizTitle
                 title={quizData.metaTitle}
@@ -54,8 +38,7 @@ const QuizDetails = ({
                 quizType={quizData.type}
                 quizMultipleChoiceContents={quizData.multipleChoiceContents}
                 />
-
-        </>
+        </QuizDetailsManager>
     );
 };
 
