e908570a21659de6aa531af18130f396fad1a079
jjalseu <cjfwns966@gmail.com>
2024-12-12 20:15:05 +0900

    - 15버전 update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 import withBundleAnalyzer from '@next/bundle-analyzer';
 
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        inlineCss: true,
+    },
+};
 
 const bundleAnalyzer = withBundleAnalyzer({
     enabled: process.env.ANALYZE === 'true',
