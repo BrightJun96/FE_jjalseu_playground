ce0046e872af1238f6f8c836bff378e3a1c27efc
jjalseu <cjfwns966@gmail.com>
2024-12-13 02:14:37 +0900

    - PrimaryLink로 교체 - 링크 > 버튼 중첩 구조 방지

--- app/(page)/quiz/[detailUrl]/_components/client/quizAnswerForm/afterCheckButtons.tsx
+++ app/(page)/quiz/[detailUrl]/_components/client/quizAnswerForm/afterCheckButtons.tsx
@@ -1,25 +1,23 @@
-import PrimaryButton from "@/app/_components/button/primaryButton";
-import useQuizHelperContext from "@/app/_context/useQuizContext";
-import {Link} from 'next-view-transitions';
+import useRandomUrl from "@/app/(page)/quiz/[detailUrl]/_helper/useRandomUrl";
+import PrimaryLink from "@/app/_components/link/primaryLink";
 import {useParams} from "next/navigation";
 import React from 'react';
 
 // 채점 후 버튼(해설, 다음문제)
 function AfterCheckButtons() {
-    const quizHelper = useQuizHelperContext();
     const {detailUrl} = useParams()
+    const randomUrl =useRandomUrl()
+
     return (
         <div className={"flex justify-center items-center gap-2 w-full"}>
-            <Link href={`/quiz/${detailUrl}/explanation`}>
-                <PrimaryButton text={"해설"} color={"primarySecondary"}/>
-            </Link>
-                <PrimaryButton
-                    text={"다음 문제"}
-                    color={"primary"}
-                    onClick={async ()=>{
-                        await quizHelper?.moveToNextQuiz(detailUrl as string)
-                    }}
-                />
+            <PrimaryLink
+                color={"primarySecondary"}
+                href={`/quiz/${detailUrl}/explanation`}>
+                해설
+            </PrimaryLink>
+             <PrimaryLink href={`/quiz/${randomUrl}`}>
+                 다음 문제
+             </PrimaryLink>
         </div>
     );
 }
--- app/_home_components/homeLink.tsx
+++ app/_home_components/homeLink.tsx
@@ -1,13 +1,15 @@
-import PrimaryButton from "@/app/_components/button/primaryButton";
-import {Link} from 'next-view-transitions';
+import PrimaryLink from "@/app/_components/link/primaryLink";
 import React from 'react';
 
 // 메인 링크
 function HomeLink() {
     return (
-        <Link href={"/quiz"}>
-            <PrimaryButton text={"퀴즈 풀어보기"} color={"primary"} className={"!w-[130px] !h-[42px]"}/>
-        </Link>
+        <PrimaryLink
+            className={"!w-[130px] !h-[42px]"}
+            href={"/quiz"}
+        >
+            퀴즈 풀어보기
+        </PrimaryLink>
     );
 }
 
