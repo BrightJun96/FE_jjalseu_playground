25485aa7f05ca8228572f1eac8c7b1ba85c23ab5
jjalseu <cjfwns966@gmail.com>
2024-12-13 02:15:19 +0900

    - 퀴즈 헬퍼 로직 변경 =-> 추후 리팩토링 예쩡

--- app/(page)/quiz/[detailUrl]/_helper/QuizHelper.ts
+++ app/(page)/quiz/[detailUrl]/_helper/QuizHelper.ts
@@ -34,11 +34,20 @@ export class QuizHelper {
 
         if (this.logicHandler.isAllQuizSolved()) {
             this.navigator.moveToCompletedPage();
-        } else {
-            const unsolvedQuiz = this.logicHandler.getUnsolvedQuiz();
-            const randomQuiz = ArrayUtils.pickRandomOne<string>(unsolvedQuiz);
-            this.navigator.moveToQuizPage(randomQuiz);
         }
+        // else {
+        //     return this.getUnsolvedQuiz();
+        // }
+    }
+    // 푼 문제 저장, 기존에 푼 문제가 있다면 추가,없다면 새로 저장
+    saveSolvedQuiz(currentQuiz: string) {
+        this.logicHandler.addSolvedQuiz(currentQuiz);
+    }
+
+    // 안 푼 문제 중 랜덤으로 하나 반환
+     getRandomOneFromUnsolvedQuiz() {
+        const unsolvedQuiz = this.logicHandler.getUnsolvedQuiz();
+        return ArrayUtils.pickRandomOne<string>(unsolvedQuiz);
     }
 
     // 현재 경로가 solvedQuizList 스토리지에 있는 url에 있는 경우, 다른 문제로 이동
@@ -47,8 +56,6 @@ export class QuizHelper {
             const unsolvedOne=  ArrayUtils.pickRandomOne<string>(this.logicHandler.getUnsolvedQuiz())
             this.navigator.moveToQuizPage(unsolvedOne);
         }
-
-
     }
 
     // 모든 퀴즈를 푼 경우, 퀴즈 완료 페이지로 이동
