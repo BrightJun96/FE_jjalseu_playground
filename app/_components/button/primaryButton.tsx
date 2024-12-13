f33fdea7fd2cc4ea02c2adbbc9361b64e42d914d
jjalseu <cjfwns966@gmail.com>
2024-12-13 21:19:04 +0900

    - text props 대신 children으로 받도록 변경 - button 내부 span 중첩 제거

--- app/_components/button/primaryButton.tsx
+++ app/_components/button/primaryButton.tsx
@@ -7,7 +7,7 @@ import React from 'react';
  */
 
 interface IPrimaryButtonProps {
-    text: string;
+    children: React.ReactNode;
     onClick?: () => void;
     className?: string;
     type?: "button" | "submit" | "reset";
@@ -15,7 +15,7 @@ interface IPrimaryButtonProps {
     color: "primary" |'primarySecondary';
 }
 
-const PrimaryButton = ({text,onClick,className,disabled,color}:IPrimaryButtonProps) => {
+const PrimaryButton = ({children,onClick,className,disabled,color}:IPrimaryButtonProps) => {
 
     const colors = {
         primary: "bg-primary-normal text-white hover:bg-primary-dark disabled:bg-primary-disabled",
@@ -23,12 +23,11 @@ const PrimaryButton = ({text,onClick,className,disabled,color}:IPrimaryButtonPro
     };
     return (
         <button
-
             className={`${colors[color]} rounded-[8px] px-[16px] text-menu flex justify-center items-center min-w-[84px] h-[32px]  ${className}`}
             onClick={onClick}
             disabled={disabled}
         >
-            <span>{text}</span>
+         {children}
         </button>
     );
 };
