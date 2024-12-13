089cfb0b922d289c2ee47c6b39883849c93c89cd
jjalseu <cjfwns966@gmail.com>
2024-12-13 23:31:29 +0900

    - 불필요 코드 제거

--- app/(page)/quiz/(page)/[detailUrl]/_helper/QuizNavigator.ts
+++ app/(page)/quiz/(page)/[detailUrl]/_helper/QuizNavigator.ts
@@ -3,24 +3,13 @@
 export class QuizNavigator {
     constructor(private router: {
         navigate:(path: string) => void,
-        prefetch?:(path: string) => void
     }) {}
 
-    // 퀴즈 페이지로 이동
-    moveToQuizPage(quizUrl: string) {
-        this.router.navigate(`/quiz/${quizUrl}`);
-        if(this.router.prefetch){
-            this.router.prefetch(`/quiz/${quizUrl}`);
-        }
-    }
 
     // 퀴즈 완료 페이지로 이동
     moveToCompletedPage() {
         this.router.navigate("/quiz/completed");
     }
 
-    // 퀴즈 시작 페이지로 이동
-    moveToStartPage() {
-        this.router.navigate("/quiz");
-    }
+
 }
