b1d5903fcbb4387907a742be97fda307e85550b5
jjalseu <cjfwns966@gmail.com>
2024-12-20 00:21:53 +0900

    -  Navigator 제거

--- app/(page)/quiz/_provider/quizHelperProvider.tsx
+++ app/(page)/quiz/_provider/quizHelperProvider.tsx
@@ -2,23 +2,18 @@
 
 import useQuizStorageContext from "@/app/(page)/quiz/_context/_hook/useQuizStorageContext";
 import quizStorageHelperContext from "@/app/(page)/quiz/_context/quizStorageHelperContext";
-import {QuizNavigator} from "@/app/(page)/quiz/_helper/QuizNavigator";
 import {QuizStorageHelper} from "@/app/(page)/quiz/_helper/QuizStorageHelper";
-import {useRouter} from "next/navigation";
 import React, {useEffect} from 'react';
 
 // 퀴즈 헬퍼 프로바이더(클라이언트용)
 function QuizStorageHelperProvider({children}:{children:React.ReactNode}) {
 
-    const router = useRouter();
     const quizStorage = useQuizStorageContext()
     const [quizHelper, setQuizHelper] = React.useState<QuizStorageHelper | null>(null);
 
     useEffect(() => {
         if(quizStorage) {
-            const navigator = new QuizNavigator({
-                navigate: (url: string) => router.push(url),
-            });
+
             const quizHelper = new QuizStorageHelper(quizStorage)
             setQuizHelper(quizHelper);
         }
