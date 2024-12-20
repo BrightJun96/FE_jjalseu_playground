e51dfcc0ee0265ce74e56aeef802abf0816bb7f9
jjalseu <cjfwns966@gmail.com>
2024-12-20 22:58:44 +0900

    - 경로 상수 > 퀴즈 상세,퀴즈 해설

--- app/_constants/paths.ts
+++ app/_constants/paths.ts
@@ -1,9 +1,11 @@
 // URL 경로를 상수로 정의
 const PATHS ={
     HOME: "/",
-    QUIZ: "/quiz",
-    QUIZ_COMPLETED: "/quiz/completed",
-    QUIZ_RANDOM: (id:string) => `/quiz/${id}`, // 동적 URL
+    QUIZ: "quiz",
+    QUIZ_COMPLETED: "quiz/completed",
+    QUIZ_RANDOM: (id:string) => `quiz/${id}`, // 퀴즈 랜덤
+    QUIZ_DETAIL: (id:string) => `quiz/${id}`, // 퀴즈 상세
+    QUIZ_EXPLANATION: (id:string) => `quiz/${id}/explanation`, // 퀴즈 설명
 };
 
 export default PATHS;
