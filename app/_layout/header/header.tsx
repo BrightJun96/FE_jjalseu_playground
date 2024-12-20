050e7b03cf42831212a8423567649e760a418b4b
jjalseu <cjfwns966@gmail.com>
2024-12-20 19:25:57 +0900

    - 헤더 > 로고 > h1 태그로 감싸주기

--- app/_layout/header/header.tsx
+++ app/_layout/header/header.tsx
@@ -35,9 +35,9 @@ const Header = () => {
         }
     }
     return (
-        <header className={"w-full h-[80px] md:h-[60px] sm:h-[60px] bg-headerBackground flex justify-between items-center lg:px-container md:px-10 sm:px-10"}>
+        <header className={"w-full h-[80px] md:h-[60px] sm:h-[60px] bg-headerBackground flex gap-10 items-center lg:px-container md:px-10 sm:px-10"}>
             {/*로고,메뉴*/}
-            <div className={"flex gap-10 items-center"}>
+            <h1>
                 <PrimaryLink
                     href={"/"}
                     color={"none"}
@@ -45,6 +45,7 @@ const Header = () => {
                 >
                    코아
                 </PrimaryLink>
+            </h1>
                 <nav>
                     <ul className={"flex gap-3 text-title3Normal text-primary-normal"}>
                         {NAVMENU.map((item, index) => (
@@ -57,9 +58,6 @@ const Header = () => {
                         ))}
                     </ul>
                 </nav>
-            </div>
-            {/* 로그인,회원가입 & 프로필 */}
-            {/*<AuthContainer/>*/}
         </header>
     );
 };
