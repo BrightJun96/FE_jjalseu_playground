64aec0b1dc28a5ab07a822f022b99b22de654158
jjalseu <cjfwns966@gmail.com>
2024-12-20 21:12:29 +0900

    - 컴포넌트명 변경

--- app/(page)/quiz/_components/quizOptionForm/quizOptions/quizFieldOption.tsx
+++ app/(page)/quiz/_components/quizOptionForm/quizOptions/quizFieldOption.tsx
@@ -4,7 +4,7 @@ import Select from "@/app/_components/select/select";
 import React from 'react';
 
 // 퀴즈 옵션 컴포넌트(분야)
-function QuizField({
+function QuizFieldOption({
     handleOptionChange,
     option
                    }:{
@@ -28,4 +28,4 @@ function QuizField({
     );
 }
 
-export default QuizField;
+export default QuizFieldOption;
