7e76f84d15503c446a179becd9fc3ae7c677d85a
jjalseu <cjfwns966@gmail.com>
2024-12-21 21:59:59 +0900

    - refactor:퀴즈 상세 컨텐츠 -> aria 속성 추가

--- app/(page)/quiz/(page)/[detailUrl]/_components/server/quizContent.tsx
+++ app/(page)/quiz/(page)/[detailUrl]/_components/server/quizContent.tsx
@@ -1,18 +1,18 @@
-"use server"
+"use server";
 
 import sanitize from "@/app/_utils/function/sanitize";
-import React from 'react';
-import 'prismjs/themes/prism.css';
+import React from "react";
+import "prismjs/themes/prism.css";
 
 // 퀴즈 내용
-function QuizContent({
-                        content
-                     }:{content:string}) {
-
+function QuizContent({ content }: { content: string }) {
     return (
         <div
             className={"w-full"}
-            dangerouslySetInnerHTML={{__html: sanitize(content)}}
+            aria-live={"polite"}
+            dangerouslySetInnerHTML={{
+                __html: sanitize(content),
+            }}
         ></div>
     );
 }
