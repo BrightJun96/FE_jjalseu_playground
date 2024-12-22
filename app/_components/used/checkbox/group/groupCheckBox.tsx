5aa234149dfd89239f021c0528d59ff9e000bf34
jjalseu <cjfwns966@gmail.com>
2024-12-22 19:22:40 +0900

    refactor: 그룹 체크박스 비즈니스 로직 분리

--- app/_components/used/checkbox/group/groupCheckBox.tsx
+++ app/_components/used/checkbox/group/groupCheckBox.tsx
@@ -1,8 +1,8 @@
 import GroupCheckBoxContainer from "@/app/_components/used/checkbox/group/groupCheckBoxContainer";
 import GroupCheckBoxContent from "@/app/_components/used/checkbox/group/groupCheckBoxContent";
-import { CheckBoxHandlerProps } from "@/app/_components/used/checkbox/single/checkbox";
+import useHandleGroupCheckBox from "@/app/_components/used/checkbox/group/hook/useHandleGroupCheckBox";
 import { primitive } from "@/app/_types/primitive";
-import React, { ReactNode, useEffect } from "react";
+import React, { ReactNode } from "react";
 
 // 체크박스 그룹 옵션
 export interface GroupCheckBoxOption {
@@ -30,37 +30,14 @@ function GroupCheckBox({
     tabIndex = 0,
     className,
 }: GroupCheckBoxProps) {
-    // 체크박스 그룹 상태
-    const [checkedList, setCheckedList] = React.useState<
-        primitive[]
-    >([]);
+    const { checkedList, groupCheckHandler } =
+        useHandleGroupCheckBox(isMultiSelect);
 
-    // 체크박스 그룹 핸들러
-    function groupCheckHandler(
-        value: CheckBoxHandlerProps,
-    ) {
-        // 다중 선택이 아닌 경우, 체크된 항목이 하나만 선택되도록
-        if (!isMultiSelect) {
-            setCheckedList([value.value]);
-            return;
-        }
-
-        // 다중 선택인 경우
-        if (value.checked) {
-            setCheckedList((prev) =>
-                Array.from(new Set([...prev, value.value])),
-            );
-        } else {
-            setCheckedList((prev) =>
-                prev.filter((v) => v !== value.value),
-            );
-        }
-    }
-
-    useEffect(() => {
-        onChange(checkedList);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [checkedList]);
+    // checkedList 변경 시, 부모 컴포넌트에게 변경된 checkedList 전달
+    // useEffect(() => {
+    //     onChange(checkedList);
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, [checkedList]);
 
     return (
         <GroupCheckBoxContainer
