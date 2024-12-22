719162ed1de7220a06603ec20a2c1f4aecb1dcb4
jjalseu <cjfwns966@gmail.com>
2024-12-22 21:41:48 +0900

    refactor: ArrayUtils.ts > 필터 함수 추가

--- app/_utils/class/ArrayUtils.ts
+++ app/_utils/class/ArrayUtils.ts
@@ -1,9 +1,10 @@
-
 // 배열 관련 유틸리티 함수
-export class ArrayUtils{
+export class ArrayUtils {
     // 배열에서 랜덤하게 하나를 뽑아 반환
     static pickRandomOne<T>(arr: T[]): T {
-        const randomIndex = Math.floor(Math.random() * arr.length);
+        const randomIndex = Math.floor(
+            Math.random() * arr.length,
+        );
         return arr[randomIndex];
     }
 
@@ -14,17 +15,23 @@ export class ArrayUtils{
 
     // 배열 길이가 0일 때, 특정 함수를 실행
     static isEmpty<T>(arr: T[]): boolean {
-      return arr.length === 0;
+        return arr.length === 0;
     }
 
     // 두 배열을 비교하여, 두 배열의 length가 같은지 비교
     static isEqualLength<T>(arrA: T[], arrB: T[]): boolean {
-      return arrA.length === arrB.length;
+        return arrA.length === arrB.length;
     }
 
     // 중복 제거
     static removeDuplicate<T>(arr: T[]): T[] {
-      return [...new Set(arr)];
+        return [...new Set(arr)];
     }
 
+    // 특정 요소 필터한 배열 반환
+    static filter<T>(arr: T[], filterElement: T): T[] {
+        return arr.filter(
+            (element) => element !== filterElement,
+        );
+    }
 }
