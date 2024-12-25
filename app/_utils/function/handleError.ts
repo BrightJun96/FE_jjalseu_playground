3f49e6c7864a93b9c7eb38893d47698517fe9945
jjalseu <cjfwns966@gmail.com>
2024-12-25 22:11:53 +0900

    error-handle: handleError.ts 함수 수정 및 적용

--- app/_utils/function/handleError.ts
+++ app/_utils/function/handleError.ts
@@ -2,46 +2,43 @@ import ArrayEmptyError from "@/app/_customError/ArrayEmptyError";
 import NullOrUndefinedError from "@/app/_customError/NullOrUndefinedError";
 import ApiError from "@/app/services/ApiError";
 
-// 에러별 메시지를 담당하는 인터페이스
-interface IErrorMessages {
-    api?: string;
-    arrayEmpty?: string;
-    nullOrUndefined?: string;
+// 에러 발생시 정보(메시지, 에러 발생 시 실행할 함수)를 담당하는 인터페이스
+interface IErrorInfo {
+    api?: Info;
+    arrayEmpty?: Info;
+    nullOrUndefined?: Info;
 }
 
-// 에러 발생 시 실행할 함수를 담당하는 인터페이스
-interface IActionsWhenError {
-    api?: () => void;
-    arrayEmpty?: () => void;
-    nullOrUndefined?: () => void;
+interface Info {
+    message: string;
+    action: () => void;
 }
+
 // 에러별 처리를 담당하는 함수
 // catch 문에서 종합적으로 error 핸들링하는 함수
 function handleError(
     error: unknown,
-    errorMessages: IErrorMessages,
-    actionsWhenError?: IActionsWhenError,
+    errorInfo: IErrorInfo,
 ) {
     if (error instanceof ApiError) {
-        error.getDetailsLog(errorMessages.api);
-        if (actionsWhenError && actionsWhenError.api) {
-            actionsWhenError.api();
+        error.getDetailsLog(errorInfo.api?.message);
+        if (errorInfo && errorInfo.api?.action) {
+            errorInfo.api.action();
         }
     } else if (error instanceof ArrayEmptyError) {
-        error.getDetailsLog(errorMessages.arrayEmpty);
-        if (
-            actionsWhenError &&
-            actionsWhenError.arrayEmpty
-        ) {
-            actionsWhenError.arrayEmpty();
+        error.getDetailsLog(errorInfo.arrayEmpty?.message);
+        if (errorInfo && errorInfo.arrayEmpty?.action) {
+            errorInfo.arrayEmpty.action();
         }
     } else if (error instanceof NullOrUndefinedError) {
-        error.getDetailsLog(errorMessages.nullOrUndefined);
+        error.getDetailsLog(
+            errorInfo.nullOrUndefined?.message,
+        );
         if (
-            actionsWhenError &&
-            actionsWhenError.nullOrUndefined
+            errorInfo &&
+            errorInfo.nullOrUndefined?.action
         ) {
-            actionsWhenError.nullOrUndefined();
+            errorInfo.nullOrUndefined.action();
         }
     } else {
         console.error("알 수 없는 에러 발생", error);
--- app/services/quiz/QuizApiHandler.ts
+++ app/services/quiz/QuizApiHandler.ts
@@ -1,15 +1,15 @@
 import ExceptionManager from "@/app/_utils/class/ExceptionManager";
+import handleError from "@/app/_utils/function/handleError";
 import {
     CustomRequestInit,
     IResponse,
 } from "@/app/services/api.types";
-import ApiError from "@/app/services/ApiError";
 import QuizApi from "@/app/services/quiz/QuizApi";
 import {
     CheckAnswerResponse,
     QuizItem,
 } from "@/app/services/quiz/types";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 // 퀴즈 API - 통신 + 예외 처리와 비즈니스 로직을 포함
 export class QuizApiHandler extends QuizApi {
@@ -30,30 +30,50 @@ export class QuizApiHandler extends QuizApi {
     // 퀴즈 전체 DetailUrl 목록 조회
     async fetchQuizDetailUrlList(
         options?: CustomRequestInit,
-    ): Promise<IResponse<string[]>> {
-        const response = await this.request<string[]>(
-            "quiz/list-detail-url",
-            {
-                method: "GET",
-                ...options,
-            },
-        );
+    ): Promise<IResponse<string[]> | undefined> {
+        try {
+            // 퀴즈 URL 목록 조회
+            const response = await this.request<string[]>(
+                "quiz/list-detail-url",
+                {
+                    method: "GET",
+                    ...options,
+                },
+            );
 
-        const { data } = response;
+            const { data } = response;
 
-        // 배열이 비어있는 경우, 예외 처리
-        ExceptionManager.throwIfArrayEmpty<string>(
-            data,
-            "퀴즈 URL 목록이 비어있습니다.",
-        );
+            // 배열이 비어있는 경우, 예외 처리
+            ExceptionManager.throwIfArrayEmpty<string>(
+                [],
+                "퀴즈 URL 목록이 비어있습니다.",
+            );
 
-        // 데이터가 없을 경우, 예외 처리
-        ExceptionManager.throwIfNullOrUndefined(
-            data,
-            "퀴즈 URL 목록이 없습니다.",
-        );
+            // 데이터가 없을 경우, 예외 처리
+            ExceptionManager.throwIfNullOrUndefined(
+                null,
+                "퀴즈 URL 목록이 없습니다.",
+            );
 
-        return response;
+            return response;
+        } catch (error) {
+            handleError(error, {
+                api: {
+                    message: "퀴즈 URL 목록 API 에러",
+                    action: () => redirect("/quiz"),
+                },
+                arrayEmpty: {
+                    message:
+                        "퀴즈 URL 목록이 비어있습니다.",
+                    action: () => redirect("/quiz"),
+                },
+                nullOrUndefined: {
+                    message:
+                        "퀴즈 URL 목록 데이터가 null 혹은 undefined입니다.",
+                    action: () => redirect("/quiz"),
+                },
+            });
+        }
     }
 
     // 퀴즈 상세 조회(상세 URL)
@@ -71,12 +91,12 @@ export class QuizApiHandler extends QuizApi {
                 },
             );
         } catch (error) {
-            if (error instanceof ApiError) {
-                error.getDetailsLog(
-                    "페이지 로드에러 - 퀴즈 상세 API 에러",
-                );
-                notFound();
-            }
+            handleError(error, {
+                api: {
+                    message: "퀴즈 상세 조회 API 에러",
+                    action: () => notFound(),
+                },
+            });
         }
     }
 }
