86bc758ea6db68de8b25655a327fb36a7578664d
jjalseu <cjfwns966@gmail.com>
2024-12-27 00:53:18 +0900

    error-handle: 개인정보처리방침 페이지

---
+++ app/(page)/privacy-policy/page.tsx
@@ -0,0 +1,13 @@
+import Container from "@/app/(page)/privacy-policy/container";
+import Korean from "@/app/(page)/privacy-policy/korean";
+import React from "react";
+
+async function Page() {
+    return (
+        <Container>
+            <Korean />
+        </Container>
+    );
+}
+
+export default Page;
