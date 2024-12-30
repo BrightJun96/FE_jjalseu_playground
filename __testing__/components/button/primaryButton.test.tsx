6feccdabc7769ae529f442dd7fea485b05d0b0fd
jjalseu <cjfwns966@gmail.com>
2024-12-30 17:13:03 +0900

    testing : testing 환경 셋업

---
+++ __testing__/components/button/primaryButton.test.tsx
@@ -0,0 +1,19 @@
+import PrimaryButton from "@/app/_components/used/button/primaryButton";
+import { render } from "@testing-library/react";
+import { expect, test } from "vitest";
+
+test("props로 전달한 disabled 값이 true일 때 버튼이 비활성화 상태가 되어야 한다.", () => {
+    // Given
+    const { getByRole } = render(
+        <PrimaryButton
+            color={"primary"}
+            disabled={true}
+        >
+            버튼
+        </PrimaryButton>,
+    );
+    const button = getByRole("button");
+    // When
+    // Then
+    expect(button).toBeDisabled();
+});
