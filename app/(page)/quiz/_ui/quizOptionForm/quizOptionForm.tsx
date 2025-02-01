0e2f9e607491c4ea2d397d074d3eb8017aa9ed3e
jjalseu <cjfwns966@gmail.com>
2025-02-01 23:25:04 +0900

    퀴즈 옵션 주석

--- app/(page)/quiz/_ui/quizOptionForm/quizOptionForm.tsx
+++ app/(page)/quiz/_ui/quizOptionForm/quizOptionForm.tsx
@@ -1,7 +1,6 @@
 "use server";
 
 import QuizOptionFormContainer from "@/app/(page)/quiz/_ui/quizOptionForm/quizOptionFormContainer";
-import QuizOptions from "@/app/(page)/quiz/_ui/quizOptionForm/quizOptions/quizOptions";
 import QuizStartButton from "@/app/(page)/quiz/_ui/quizOptionForm/quizStartButton";
 import React from "react";
 
@@ -10,7 +9,7 @@ function QuizOptionForm() {
     return (
         <QuizOptionFormContainer>
             {/*옵션*/}
-            <QuizOptions />
+            {/*<QuizOptions />*/}
             {/*시작하기 버튼*/}
             <QuizStartButton>퀴즈 시작하기</QuizStartButton>
         </QuizOptionFormContainer>
